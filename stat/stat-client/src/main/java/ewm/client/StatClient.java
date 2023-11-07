0a619f5f5bf5bd54947a5dcb05d255e193161b59
Canoutis <rosoutis@gmail.com>
2023-11-07 23:05:01 +0300

    fix: reviewer remarks and pom for netty on m1

--- stat/stat-client/src/main/java/ewm/client/StatClient.java
+++ stat/stat-client/src/main/java/ewm/client/StatClient.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-@Component
+@Service
 public class StatClient {
     private final WebClient.Builder webClientBuilder;
     private final String statServerBaseUrl;
@@ -24,26 +26,29 @@ public class StatClient {
         this.statServerBaseUrl = statServerBaseUrl;
     }
 
-    public ResponseEntity<List<RequestHitInfoDto>> getRequestHitInfoDto(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        StringBuilder uri = new StringBuilder(statServerBaseUrl + "/stats?start=" + start + "&end=" + end + (unique != null ? unique : ""));
+    public List<RequestHitInfoDto> getRequestHitInfoDto(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        StringBuilder uri = new StringBuilder(statServerBaseUrl + "/stats?start=" +
+                URLEncoder.encode(start.format(dateTimeFormatter), StandardCharsets.UTF_8) + "&end=" +
+                URLEncoder.encode(end.format(dateTimeFormatter), StandardCharsets.UTF_8) +
+                (unique != null ? "&" + unique : ""));
         if (uris != null) uris.forEach(element -> uri.append("&uris=").append(element));
         return webClientBuilder.build()
                 .get()
                 .uri(uri.toString())
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<ResponseEntity<List<RequestHitInfoDto>>>() {
-                })
-                .block();
+                .bodyToMono(new ParameterizedTypeReference<List<RequestHitInfoDto>>() {
+                }).block();
     }
 
-    public ResponseEntity<RequestHitDto> hitRequest(RequestHitDto requestHitDto) {
+    public RequestHitDto hitRequest(RequestHitDto requestHitDto) {
         return webClientBuilder.build()
                 .post()
                 .uri(statServerBaseUrl + "/hit")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(BodyInserters.fromValue(requestHitDto))
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<ResponseEntity<RequestHitDto>>() {
+                .bodyToMono(new ParameterizedTypeReference<RequestHitDto>() {
                 })
                 .block();
     }
